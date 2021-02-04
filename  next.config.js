66d5c9fb4e0d28c10bb42ee462f5f938b58c1008
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-04 11:35:47 +0530

    next.config

---
+++  next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+
+    // Example using webpack option
+    //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    config.node = {
+      fs: "empty",
+      net: "empty",
+      tls: "empty",
+    };
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
+    return config;
+  },
+};
